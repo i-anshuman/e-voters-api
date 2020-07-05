0aaef2006dcc9f78869d4092cad401f89a28f640
Anshuman Gupta <anshuman844@gmail.com>
2020-07-05 12:27:07 +0530

    feat: Add verification module It has functions to verify inputs incomming.

---
+++ utils/verification.js
@@ -0,0 +1,39 @@
+const isName = name => /^[A-Za-z]+([ ]?[A-Za-z]+)+$/.test(name);
+
+const isRelation = relation => /^[A-Z a-z]+$/.test(relation);
+
+const isDate = date => /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/.test(date);
+
+const isGender = gender => /^(male|female|third gender)$/i.test(gender);
+
+const isHouseNumber = houseNumber => /^\d+$/.test(houseNumber);
+
+const isPin = pin => /^[1-9]\d{5}$/.test(pin);
+
+const isState = state => /^[A-Za-z]+([ ]?[A-Za-z0-9]+)+$/.test(state);
+
+const isLocality = locality => /^[A-Za-z]+([ ]?[A-Za-z0-9,.-]+)+$/.test(locality);
+
+const isEmail = email => /^\w+@\w+\.\w+$/.test(email);
+
+const isMobile = mobile => /^\d{3}-\d{3}-\d{4}$/.test(mobile);
+
+const isEmpty = str => !str || /^\s*$/.test(str);
+
+module.exports = {
+  isName,
+  isRelation,
+  isDate,
+  isGender,
+  isHouseNumber,
+  isLocality,
+  isBlock: isName,
+  isPost: isName,
+  isPin,
+  isDistrict: isState,
+  isState,
+  isEmail,
+  isMobile,
+  isString: isName,
+  isEmpty
+};
