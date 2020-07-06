317356081d5e97fa9df2d311db24b552276baf00
Anshuman Gupta <anshuman844@gmail.com>
2020-07-06 15:16:35 +0530

    feat: Add method enroll It save new voter's information to database.

---
+++ controllers/enroll.js
@@ -0,0 +1,12 @@
+const Voter = require('../models/voter');
+
+const enroll = voterInfo => {
+  return new Promise((resolve, reject) => {
+    const voter = new Voter(voterInfo);
+    voter.save((error, result) => {
+      error ? reject(error) : resolve(result);
+    })
+  });
+}
+
+module.exports = enroll;
